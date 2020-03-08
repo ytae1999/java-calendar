001755b2d63b9963f1e9d4b624d3aa780d9eecab
ytae1999 <48212267+ytae1999@users.noreply.github.com>
2020-03-08 13:09:06 +0900

    New InputNumberCalendar class and main

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -11,8 +11,7 @@ public class Calendar {
 		System.out.println("1   2  3  4  5  6  7");
 		System.out.println("8   9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-
+		System.out.println("22 23 24 25 26 27 28");		
 	}
 
 }
---
+++ src/honux/calendar/InputNumberCalendar.java
@@ -0,0 +1,48 @@
+package honux.calendar;
+
+import java.util.Scanner;
+
+public class InputNumberCalendar {
+	
+	//다른 방법으로 풀이하기
+	//System.out.print("달을 입력하세요: ");
+	//
+	//Scanner scanner = new Scanner(System.in);
+	//int month = scanner.nextInt();
+	//int[] lastMonthDates = {31, 28, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31};
+	//System.out.println(month + "월은 " + lastMonthDates[month - 1] + "까지 있습니다.");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+
+		System.out.print("달을 입력하세요: ");
+
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+		case 11:
+			System.out.println(month + "월은 31일까지 있습니다.");
+			break;
+		case 2:
+			System.out.println(month + "월은 28일까지 있습니다.");
+			break;
+		case 4:
+		case 6:
+		case 8:
+		case 10:
+		case 12:
+			System.out.println(month + "월은 30일까지 있습니다.");
+			break;
+
+		}
+		scanner.close();
+	}
+
+}
