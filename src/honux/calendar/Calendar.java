2152caec6ccd2ea43b4a3b37f08dd691c28bd3fe
ytae1999 <48212267+ytae1999@users.noreply.github.com>
2020-03-08 11:04:31 +0900

    property change

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -6,7 +6,7 @@ public class Calendar {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Calendar");
 		System.out.println("");
-		System.out.println("ÀÏ     ¿ù    È­   ¼ö    ¸ñ    ±İ    Åä ");
+		System.out.println("ì¼      ì›”    í™”   ìˆ˜    ëª©     ê¸ˆ    í† ");
 		System.out.println("--------------------");
 		System.out.println("1   2  3  4  5  6  7");
 		System.out.println("8   9 10 11 12 13 14");
