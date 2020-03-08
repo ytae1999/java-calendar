a49bf8fffda319d08e76936fe1f29f6e9a344fa4
ytae1999 <48212267+ytae1999@users.noreply.github.com>
2020-03-08 14:44:18 +0900

    Input repeat time to print

--- src/honux/calendar/FunctionCalendar.java
+++ src/honux/calendar/FunctionCalendar.java
@@ -7,22 +7,26 @@ public class FunctionCalendar {
 	private final int[] last_Month_Dates = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int lastMonthDates(int month) {
-		
+
 		return last_Month_Dates[month - 1];
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.print("달을 입력하세요: ");
 		Scanner scanner = new Scanner(System.in);
-		int month = scanner.nextInt();
-
 		FunctionCalendar cal = new FunctionCalendar();
 
-		System.out.println(month + "월은" + cal.lastMonthDates(month) + "일까지 있습니다.");		
+		System.out.print("횟수를 입력하세요: ");
+		int repeatNumber = scanner.nextInt();
+
+		for (int i = 0; i < repeatNumber; i++) {
+			System.out.print("달을 입력하세요: ");
+			int month = scanner.nextInt();
+
+			System.out.println(month + "월은" + cal.lastMonthDates(month) + "일까지 있습니다.");
+		}
+		System.out.println("종료하겠습니다.");
 		scanner.close();
 	}
-
 }
